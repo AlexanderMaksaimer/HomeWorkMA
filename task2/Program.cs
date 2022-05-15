70433e8ae6a7d7359834fc7aebf0cf4a44d51199
«Aleksander <pavel4uk18@gmail.com>
2022-05-15 18:17:57 +0300

    task2ready

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число: ");
+string NumberStr = Console.ReadLine();
+
+int number = int.Parse(NumberStr);
+
+if (number>99)
+{
+    Console.WriteLine("Третья цифра заданного числа: ");
+    Console.WriteLine(number.ToString()[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры в заданном числе нет");
+}
